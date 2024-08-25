e991060f8f9914990e89445c7cf5af3ec959bc93
dineshgitclone <116178552+dineshgitclone@users.noreply.github.com>
2024-08-25 21:26:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/dineshgitclone/RRbot.git /RRbot
+  git clone https://github.com/dineshgitclone/adlinkfly-telegram-bot.git /adlinkfly-telegram-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /RRbot
+  git clone $UPSTREAM_REPO /adlinkfly-telegram-bot
 fi
-cd /RRbot
+cd /adlinkfly-telegram-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
